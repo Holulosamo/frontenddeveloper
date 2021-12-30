a864fb8e0cd3344119e034ef837f40f56f8756a8
Holulosamo <eliasrodriguez000@gmail.com>
2021-12-30 16:21:34 -0300

    nav menu adding

--- js/index.js
+++ js/index.js
@@ -3,22 +3,22 @@ const d = document,
     projectsItems = d.querySelectorAll('.projects__item'),
     modal = d.createElement('div'),
     fragment = d.createDocumentFragment(),
-    image = d.createElement("img");
+    image = d.createElement("img"),
+    navBtn = d.querySelector('.hamburger'),
+    navMenu = d.querySelector('.nav');
 
 
 projectsItems.forEach(el => {
     el.addEventListener("click", (e) => {
-        console.log(el);
-
         modal.className = 'modal';
         image.className = 'modal__image';
         image.src = el.querySelector('img').getAttribute('src');
 
         fragment.appendChild(image);
         modal.appendChild(fragment);
+        body.style.overflowY = 'hidden';
         body.appendChild(modal);
 
-        body.style.overflowY = 'hidden';
     });
 });
 
@@ -28,4 +28,9 @@ d.addEventListener('click', (e) => {
         body.removeChild(modal);
         body.style.overflowY = "auto";
     }
+});
+
+navBtn.addEventListener('click', (e) => {
+    navBtn.classList.toggle('is-active');
+    navMenu.classList.toggle("nav-active");
 });
